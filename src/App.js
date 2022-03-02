f27c0abb93489c603a589e878f022d73c1dbc8bf
F100P <timmy449@student.liu.se>
2022-03-02 14:00:49 +0100

    Igen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import getTestData from './connections/getTestData.js';
-
+import { PieChart,Pie } from 'recharts';
 const App = () => {
   const [testData, setTestData] = useState([]);
 
@@ -10,7 +10,38 @@ const App = () => {
     });
   }, []);
 
-  return <div>Hej {testData.PERSONNUMMER}</div>;
+  return renderPieChart;
 };
 
 export default App;
+
+
+const data = [{
+  "name": "5a",
+  "value": 400
+
+},
+{
+  "name": "3a",
+  "value": 300
+},
+{
+  "name": "4a",
+  "value": 300
+},
+{
+  "name": "U",
+  "value": 200
+},
+{
+  "name": "U",
+  "value": 2000
+}
+];
+
+const renderPieChart= (
+  <PieChart width={730} height={250}>
+  <Pie data={data} dataKey="value" nameKey="3a" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+  
+</PieChart>
+);
