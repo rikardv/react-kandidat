dd85e57eadd8100509da568ae375c2808d8aca6e
Rikard Vestergaard <rikve590@student.liu.se>
2022-03-02 15:51:22 +0100

    pie chart färg

--- src/App.js
+++ src/App.js
@@ -4,28 +4,18 @@ import PieChartComponent from './charts/PieChartComponent.js';
 const App = () => {
   const [testData, setTestData] = useState();
 
- 
-
   useEffect(() => {
     getTestData().then((res) => {
       setTestData(res.data);
       console.log(res.data);
     });
   }, []);
-  
-  
 
-
-  return (<PieChartComponent data= {testData}></PieChartComponent>)
+  return (
+    <div>
+      {testData && <PieChartComponent data={testData}></PieChartComponent>}
+    </div>
+  );
 };
 
-
-
 export default App;
-
-
-
-
-
-  
-
--- src/charts/PieChartComponent.js
+++ src/charts/PieChartComponent.js
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { PieChart,Pie,ResponsiveContainer, } from 'recharts';
+import { PieChart, Pie, ResponsiveContainer, Cell } from 'recharts';
 
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#202409'];
 
-
-const PieChartComponent = data => {
-    const [newData, setNewData] = useState([])
-    console.log(data)
-    useEffect(() => {
-      setNewData(data.data)
-    }, [data])
-    return (
-        <ResponsiveContainer width="100%" height={450}> 
-        <PieChart width={730} height={250}>
-        
-        <Pie data={newData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+const PieChartComponent = (data) => {
+  const [newData, setNewData] = useState([]);
+  console.log(data);
+  useEffect(() => {
+    setNewData(data.data);
+  }, [data]);
+  return (
+    <ResponsiveContainer width="100%" height={450}>
+      <PieChart width={2000} height={2000}>
+        <Pie
+          data={newData}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          outerRadius={200}
+          fill="#8884d8"
+          dataKey="antal"
+        >
+          {newData.map((entry, index) => (
+            <Cell
+              onClick={() => console.log(entry)}
+              key={`cell-${index}`}
+              fill={COLORS[index % COLORS.length]}
+            />
+          ))}
+        </Pie>
       </PieChart>
     </ResponsiveContainer>
-    )
-}
+  );
+};
 
-  export default PieChartComponent;
+export default PieChartComponent;
