05f19b65c4a6599f7c44d3926b1a0aa8d9c10579
Rikard Vestergaard <rikve590@student.liu.se>
2022-03-29 16:04:09 +0200

    stylesheet och lekkomponent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import getBetyg from './connections/test/getBetyg.js';
 import PieChartComponent from './components/charts/PieChart.js';
 import getAvbrott from './connections/test/getAvbrott.js';
 import Histogram from './components/charts/Histogram.js';
+import theme from './style/themeprovider';
+import { ThemeProvider } from '@mui/material/styles';
+import { Typography } from '@mui/material';
+import SandBox from './components/layout/SandBox.js';
+
 const App = () => {
   const [betyg, setBetyg] = useState();
   const [avbrott, setAvbrott] = useState();
@@ -19,10 +24,13 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      {betyg && <PieChartComponent data={betyg}></PieChartComponent>}
-      {avbrott && <Histogram data={avbrott} />}
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <SandBox />
+        {betyg && <PieChartComponent data={betyg}></PieChartComponent>}
+        {avbrott && <Histogram data={avbrott} />}
+      </div>
+    </ThemeProvider>
   );
 };
 
---
+++ src/style/themeprovider.js
@@ -0,0 +1,34 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Mulish', sans-serif",
+    h1: {
+      fontSize: 30,
+    },
+
+    h2: {
+      fontSize: 20,
+    },
+
+    h3: {
+      fontSize: 15,
+    },
+
+    body: {
+      fontSize: 12,
+    },
+  },
+
+  palette: {
+    primary: {
+      main: '#11636C',
+    },
+
+    secondary: {
+      main: '#E8B49ECC',
+    },
+  },
+});
+
+export default theme;
