d7b17380c18c1fac49cc3d537802a3e0d1d76ff2
Rikard Vestergaard <rikve590@student.liu.se>
2022-04-02 11:52:41 +0200

    Deleted props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Dagar from './components/charts/Dagar.js';
 
 const App = () => {
-  return <Dagar kurs="TNA001" startDatum="2021-08-17" />;
+  return <Dagar />;
 };
 
 export default App;
