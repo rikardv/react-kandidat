43af4c5e6f006e5e68ffde1371bbe446f812ebb7
Rikard Vestergaard <rikve590@student.liu.se>
2022-04-04 22:24:26 +0200

    Disabled feature

--- src/components/charts/DatesList.js
+++ src/components/charts/DatesList.js
@@ -8,23 +8,40 @@ const DatesList = ({ kurskod, handleSelectDate }) => {
   const [selectedDate, setSelectedDate] = useState('');
 
   useEffect(() => {
-    getCourseStartDatesjs(kurskod).then((dates) => {
-      setDates(dates.data);
+    getCourseStartDatesjs(kurskod).then((data) => {
+      //Create arr and add selected property
+      let arr = data.data.map((element) => ({ ...element, disabled: false }));
+      setDates(arr);
     });
   }, []);
 
   const handleSelect = (e) => {
+    console.log(e.target);
     setSelectedDate(e.target.value);
     handleSelectDate(e.target.value);
   };
 
+  const handleDisabled = (indx) => {
+    let newArr = [...dates];
+    let item = { ...newArr[indx] };
+    item.disabled = !item.disabled;
+    newArr[indx] = item;
+    setDates(newArr);
+  };
+
   return (
     <FormControl style={{ minWidth: 150 }}>
-      <InputLabel id="demo-simple-select-label">Välj kursstart</InputLabel>
-      <Select label="Kursstart" value={selectedDate} onChange={handleSelect}>
+      <InputLabel id='demo-simple-select-label'>Välj kursstart</InputLabel>
+      <Select label='Kursstart' value={selectedDate} onChange={handleSelect}>
         {dates &&
-          dates.map((res) => (
-            <MenuItem value={res.start_datum}>{res.start_datum}</MenuItem>
+          dates.map((res, indx) => (
+            <MenuItem
+              value={res.start_datum}
+              disabled={res.disabled}
+              onClick={() => handleDisabled(indx)}
+            >
+              {res.start_datum}
+            </MenuItem>
           ))}
       </Select>
     </FormControl>
