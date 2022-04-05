58a9a2d145b701baa069be6d169105ffb9e918e1
Rikard Vestergaard <rikve590@student.liu.se>
2022-04-05 21:05:09 +0200

    Update getSlapande.js

--- src/connections/getSlapande.js
+++ src/connections/getSlapande.js
@@ -1,8 +1,6 @@
 export default async function (programkod, startdatum) {
   var slapande;
 
-  console.log('hallå?');
-
   await fetch(
     'http://localhost:8080' +
       `/program/slapande?program=${programkod}&startdatum=${startdatum}`,
