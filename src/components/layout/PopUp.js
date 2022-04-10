2496cd36374ef6a60197a4b94202445c61c8d080
Max Eriksson <maxer142@student.liu.se>
2022-04-10 17:40:46 +0200

    När man väljer ett program / en kurs flyttas den över till listan över valda. När man tar bort den från listan över valda så läggs den tillbaka i den tidigare listan.

--- src/components/layout/PopUp.js
+++ src/components/layout/PopUp.js
@@ -16,15 +16,24 @@ import CloseIcon from '@mui/icons-material/Close';
 const PopUp = (props) => {
   const { onClose, open, data, titel } = props;
 
+  //Stäng pop-upen.
   const handleClose = () => {
     onClose();
   };
 
-  const [selected, setSelected] = useState([]);
+  const [selected, setSelected] = useState([]); //För att lagra valda program /kurser.
+  const [newList, setNewList] = useState(data); //skapa ny lista för att kunna göra justeringar i den.
 
-  const handleSelection = (e, program) => {
-    setSelected({ program });
-    console.log(selected);
+  //När man väljer ett program/kurs... så läggs elementet till i listan över valda och tas bort från listan över valbara.
+  const handleSelection = (element) => {
+    setSelected([...selected, element]);
+    setNewList(newList.filter((item) => item !== element));
+  };
+
+  //När man tar bort ett program/kurs... så läggs elementet till i listan över valbara och tas bort från listan över valda.
+  const handleDelete = (element) => {
+    setNewList([...newList, element]);
+    setSelected(selected.filter((item) => item !== element));
   };
 
   return (
@@ -40,32 +49,41 @@ const PopUp = (props) => {
         </Typography>
       </DialogTitle>
       <Divider />
-      <Stack marginTop={3} direction='row' spacing={3}>
-        {data.map((program) => (
-          <>
-            <Chip
-              style={{ marginBottom: 3, marginLeft: 15 }}
-              variant='outlined'
-              key={program}
-              clickable={true}
-              label={
-                <Typography variant='h3' fontWeight='medium'>
-                  {program}
-                </Typography>
-              }
-              onClick={(e) => handleSelection(e, program)}
-              onDelete={(e) => handleSelection(e, program)}
-              deleteIcon={<AddIcon />}
-            ></Chip>
-          </>
-        ))}
-      </Stack>
-      <TextField
-        label={'Sök ' + titel}
-        variant='outlined'
-        size='small'
-        style={{ margin: 15 }}
-      />
+      {newList.length == 0 ? (
+        <Typography>Alla {titel} Är Valda</Typography>
+      ) : (
+        <Stack marginTop={3} direction='row' spacing={3}>
+          {newList.map((element) => (
+            <>
+              <Chip
+                style={{ marginBottom: 3, marginLeft: 15 }}
+                variant='outlined'
+                key={element}
+                clickable={true}
+                label={
+                  <Typography variant='h3' fontWeight='medium'>
+                    {element}
+                  </Typography>
+                }
+                onClick={() => handleSelection(element)}
+                onDelete={() => handleSelection(element)}
+                deleteIcon={<AddIcon />}
+              ></Chip>
+            </>
+          ))}
+        </Stack>
+      )}
+
+      {newList.length == 0 ? (
+        <></>
+      ) : (
+        <TextField
+          label={'Sök ' + titel}
+          variant='outlined'
+          size='small'
+          style={{ margin: 15 }}
+        />
+      )}
       <Divider />
       <DialogTitle>
         <Typography
@@ -78,24 +96,31 @@ const PopUp = (props) => {
         </Typography>
       </DialogTitle>
       <Divider />
-      <Stack marginTop={3} direction='row' spacing={3}>
-        {data.map((program) => (
-          <>
-            <Chip
-              style={{ marginBottom: 14, marginLeft: 15 }}
-              variant='outlined'
-              clickable={true}
-              label={
-                <Typography variant='h3' fontWeight='medium'>
-                  {program}
-                </Typography>
-              }
-              onDelete={(e) => handleSelection(e, program)}
-              deleteIcon={<ClearIcon />}
-            ></Chip>
-          </>
-        ))}
-      </Stack>
+      {selected.length == 0 ? (
+        <Typography>
+          Inga Valda {titel}, Var God Välj {titel} Ovan!
+        </Typography>
+      ) : (
+        <Stack marginTop={3} direction='row' spacing={3}>
+          {selected.map((element) => (
+            <>
+              <Chip
+                style={{ marginBottom: 14, marginLeft: 15 }}
+                variant='outlined'
+                clickable={true}
+                label={
+                  <Typography variant='h3' fontWeight='medium'>
+                    {element}
+                  </Typography>
+                }
+                onDelete={() => handleDelete(element)}
+                onClick={() => handleDelete(element)}
+                deleteIcon={<ClearIcon />}
+              ></Chip>
+            </>
+          ))}
+        </Stack>
+      )}
       <Button
         style={{
           marginBottom: 15,
