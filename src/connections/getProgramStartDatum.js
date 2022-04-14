fc2a1e1ba13927c30dcbe4d9aa89dc753c9f7103
Max Eriksson <maxer142@student.liu.se>
2022-04-14 14:57:51 +0200

    Nu hämtas alla startdatum för valda program.

--- src/connections/getProgramStartDatum.js
+++ src/connections/getProgramStartDatum.js
@@ -1,15 +1,9 @@
-export default async function (programkod) {
-  /**
-   * Hämtar personer med släpande kurser med program och startdatum som parameter
-   */
+export default async function (program) {
   var datum;
 
-  await fetch(
-    'http://localhost:8080' + `/program/datum?program=${programkod}`,
-    {
-      method: 'GET',
-    }
-  )
+  await fetch('http://localhost:8080' + `/program/datum?${program}`, {
+    method: 'GET',
+  })
     .then((response) => response.json())
     .then((response) => {
       datum = response;
