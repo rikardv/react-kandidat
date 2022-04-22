2af06ca7a296604eea2dcad68fdc2213aa140ff8
Max Eriksson <maxer142@student.liu.se>
2022-04-22 12:06:24 +0200

    Visar flera kurser i samma graf. Tryck på label för att dölja kurs.

--- src/components/charts/DagarNew.js
+++ src/components/charts/DagarNew.js
@@ -27,6 +27,21 @@ const DagarNew = ({ kurskod, startDatum }) => {
     });
   }, [kurskod, startDatum]);
 
+  //Hide course when clicked on label.
+  const handleActive = (e) => {
+    console.log(e.value);
+    for (var i = 0; i < dagar.length; i++) {
+      console.log(dagar[i].kurs);
+      if (dagar[i].kurs == e.value) {
+        let newArr = [...dagar];
+        let item = { ...newArr[i] };
+        item.active = !item.active;
+        newArr[i] = item;
+        setDagar(newArr);
+      }
+    }
+  };
+
   return (
     <Grid
       display='flex'
@@ -35,45 +50,53 @@ const DagarNew = ({ kurskod, startDatum }) => {
       justifyContent='space-evenly'
       width='100%'
     >
-      {dagar &&
-        dagar.map((res, indx) => (
-          <Card
-            style={{
-              width: '90%',
-              height: 'auto',
-            }}
-          >
-            <CardContent>
-              <Typography variant='h1' fontWeight='medium' align='center'>
-                TEMP
-              </Typography>
-              <ResponsiveContainer height={500} width='100%'>
-                <LineChart data={res.data}>
-                  <CartesianGrid horizontal={false} vertical={false} />
-                  <XAxis
-                    dataKey='antalDagar'
-                    label={{
-                      value: 'Antal dagar till avslutad kurs',
-                      position: 'insideBottom',
-                    }}
-                    domain={[0, 'dataMax']}
-                    height={40}
+      <Card
+        style={{
+          width: '90%',
+          height: 'auto',
+        }}
+      >
+        <CardContent>
+          <Typography variant='h1' fontWeight='medium' align='center'>
+            Antal dagar till avklarad kurs
+          </Typography>
+          <ResponsiveContainer height={500} width='100%'>
+            <LineChart>
+              <CartesianGrid horizontal={false} vertical={false} />
+              <XAxis
+                dataKey='antalDagar'
+                label={{
+                  value: 'Antal dagar till avslutad kurs',
+                  position: 'insideBottom',
+                }}
+                domain={[0, 'dataMax']}
+                height={40}
+              />
+              <YAxis
+                domain={[0, 100]}
+                label={{
+                  value: 'Procent',
+                  angle: -90,
+                  position: 'insideLeft',
+                }}
+              />
+              <Legend verticalAlign='top' height={30} onClick={handleActive} />
+              {dagar &&
+                dagar.map((res, indx) => (
+                  <Line
+                    type='monotone'
+                    dataKey='andelProcent'
+                    data={res.data}
+                    name={res.kurs}
+                    hide={res.active}
+                    dot={false}
+                    connectNulls
                   />
-                  <YAxis
-                    domain={[0, 100]}
-                    label={{
-                      value: 'Procent',
-                      angle: -90,
-                      position: 'insideLeft',
-                    }}
-                  />
-
-                  <Line dataKey='andelProcent' />
-                </LineChart>
-              </ResponsiveContainer>
-            </CardContent>
-          </Card>
-        ))}
+                ))}
+            </LineChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
     </Grid>
   );
 };
